e9306fa3cf7da253cb97dac395f82f856b8f95fb
Ifti Khan <ifti_k@hotmail.com>
2021-06-01 18:36:15 +0100

    mod: added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IxbDLCRG95yajRjqSQaJAPgH6FWF6WpCeAczVyZbXWvwIdZY5ffdyXM6FprEZfKZ3DbvkdDBxhR9MGFXP4In9cX00xZOpISzF',
+        'client_secret': 'test client secret',
+
     }
 
     return render(request, template, context)
