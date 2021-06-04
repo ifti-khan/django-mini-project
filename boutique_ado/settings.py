78c24f6aedea1a9c77e1f9f6d8260f7166fc65da
Ifti Khan <ifti_k@hotmail.com>
2021-06-04 18:24:46 +0100

    removed secret key and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['iftikhan-django-mini-project.herokuapp.com/', 'localhost', '127.0.0.1']
 
