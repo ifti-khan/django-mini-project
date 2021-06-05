22e4ca28c85551b3b41d316e5af7725dbd2d7243
Ifti Khan <ifti_k@hotmail.com>
2021-06-05 21:17:26 +0100

    fix: settings dev_evn fix

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 # reading .env file
 environ.Env.read_env()
 
-dev_env = os.environ.get('DEVELOPMENT', True)
+dev_env = env("DEVELOPMENT")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
